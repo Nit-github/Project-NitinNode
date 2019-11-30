d98ae624981210f0e9d9f1fff11a772a6d82261d
Nitin Kumar <shukla.sameer33@gmail.com>
2019-11-30 17:30:30 +0530

    Already updated

--- GlobalData/globalvar.js
+++ GlobalData/globalvar.js
@@ -4,4 +4,3 @@ window.onload = (function(){ document.getElementById("loader").src="https://thum
 const emailtepmlate=/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 function isNumberKey(e) { var t = e.which ? e.which : event.keyCode; if ((t >= 48 && t <= 57) || (t >= 96 && t <= 105) || (t == 8 || t == 9 || t == 127 || t == 37 || t == 39 || t == 46)) return true; return false; }
 function AvoidZero(v) {  var V = document.getElementById(v).value; return (V.charAt(0) == 0) ? (document.getElementById(v).value = V.substring(1, V.length), false) : false; }
-function clearvalue(v){$("#"+v).val("");}
