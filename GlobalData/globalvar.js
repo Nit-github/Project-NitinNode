690e78325ce97b4bb408299830d5ed8988042daf
Nit-github <sameer33@gmail.com>
2019-12-26 02:29:08 -0800

    s

--- GlobalData/globalvar.js
+++ GlobalData/globalvar.js
@@ -1,5 +1,5 @@
-const ajxurl=`http://localhost:4000`;
-//const ajxurl=`https://nit-node.herokuapp.com`;
+//const ajxurl=`http://localhost:4000`;
+const ajxurl=`https://nit-node.herokuapp.com`;
 window.onload = (function(){ document.getElementById("loader").src="https://thumbs.gfycat.com/MarriedMarvelousHawaiianmonkseal-small.gif";});
 const emailtepmlate=/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 function isNumberKey(e) { var t = e.which ? e.which : event.keyCode; if ((t >= 48 && t <= 57) || (t >= 96 && t <= 105) || (t == 8 || t == 9 || t == 127 || t == 37 || t == 39 || t == 46)) return true; return false; }
